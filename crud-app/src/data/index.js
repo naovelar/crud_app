510750d607d2cb8858fff9b5bafe822ff771c9ee
nhporti2 <nhporti2@gmail.com>
2021-07-02 22:14:57 -0500

    Add files via upload

---
+++ crud-app/src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Susan',
+    lastName: 'Jensen',
+    email: 'susan@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Abigail',
+    lastName: 'Danvers',
+    email: 'abigail@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Richard',
+    lastName: 'Hanson',
+    email: 'richard@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Kenneth',
+    lastName: 'Rosner',
+    email: 'kenneth@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Alfredo',
+    lastName: 'Guiseppo',
+    email: 'alfredo@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Lauren',
+    lastName: 'Lagerway',
+    email: 'lauren@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'George',
+    lastName: 'Thomspon',
+    email: 'george@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'Jessica',
+    lastName: 'Wellington',
+    email: 'jessica@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Matthew',
+    lastName: 'Warren',
+    email: 'matthew@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Lyndsey',
+    lastName: 'Follette',
+    email: 'lyndsey@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
